aea577e43e7ae32e138bced57384a4cf2f85b52e
avolent <Will@slamped.com>
2020-05-27 21:44:39 +1000

    Removing non essential stuff

--- archinstall.sh
+++ archinstall.sh
@@ -7,11 +7,11 @@ TIMEZONE="Sydney/Europe"
 #Install Preparation
 echo "Starting Installation"
 echo "Checking EFI"
-ls /sys/firmware/efi/efivars 
+#ls /sys/firmware/efi/efivars
 echo "Checking Network"
-ping archlinux.org 
+#ping archlinux.org
 echo "Setting Time"
-timedatectl set-ntp true 
+#timedatectl set-ntp true
 #Drive Preparation
 echo "Preparing drives and creating partitions"
 dd if=/dev/zero of=/dev/$DRIVE  bs=512  count=1
@@ -19,7 +19,7 @@ parted /dev/$DRIVE mklabel gpt
 parted -a opt /dev/$DRIVE mkpart primary fat32 2 512
 parted /dev/$DRIVE set 1 esp on
 parted -a opt /dev/$DRIVE mkpart primary 512 100%
-mkfs.fat -F32 /dev/${DRIVE}1 
+mkfs.fat -F32 /dev/${DRIVE}1
 mkfs.ext4 /dev/${DRIVE}2
 #Updating and syncing mirrorlist
 echo "Updating and syncing mirrorlist"
@@ -30,10 +30,7 @@ reflector -c "AUS" -f 5 -l 5 -n 5 --save /etc/pacman.d/mirrorlist
 #Installing Arch
 echo "Installing Arch"
 mount /dev/sda2 /mnt
-pacstrap /mnt base linux linux-firmware vim nano 
+pacstrap /mnt base linux linux-firmware vim nano
 genfstab -U /mnt >> /mnt/etc/fstab
 echo "Chroot into Arch"
 arch-chroot /mnt
-
-
-
