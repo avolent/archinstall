3ce7a5a6cccb5cdb5df9c8bbceaa31b75f1298ed
avolent <Will@slamped.com>
2020-05-31 16:27:34 +1000

    test

--- archinstall.sh
+++ archinstall.sh
@@ -5,28 +5,30 @@ read -n 1 -r -s -p $'Press enter to continue...\n'
 USERACC="will"
 DEVICE="will-laptop" #Hostname of the device
 DRIVE="sda"
-TIMEZONE="Australia/Sydney"
+TIMEZONE=/Australia/Sydney
+echo $TIMEZONE
+read -n 1 -r -s -p $'Press enter to continue...\n'
 # Install Preparation
-echo -e "---- Starting Installation ---- \n"
+echo -e "\n---- Starting Installation ----"
 timedatectl set-ntp true
-echo -e "---- Checking EFI ---- \n"
+echo -e "\n---- Checking EFI ----"
 if [ -d /sys/firmware/efi/efivars ]
-    then
-        echo "---- EFI enabled, continuing. ----"
-    else
-        echo "!!!! EFI not enable, halting script !!!!" 
-        exit 
+then
+    echo "---- EFI enabled, continuing. ----"
+else
+    echo "!!!! EFI not enable, halting script !!!!"
+    exit
 fi
-echo -e "---- Checking network connectivity. ----\n"
+echo -e "\n---- Checking network connectivity. ----"
 if ping -c 1 archlinux.org &> /dev/null
-    then   
-        echo "Internet Connectivity Working."
-    else   
-        echo "!!!! No Internet Connectivity. Halting script !!!!"
-        exit
+then
+    echo "Internet Connectivity Working."
+else
+    echo "!!!! No Internet Connectivity. Halting script !!!!"
+    exit
 fi
 # Drive Preparation
-echo -e "---- Preparing drives and creating partitions ----"
+echo -e "\n---- Preparing drives and creating partitions ----"
 read -n 1 -r -s -p $'Selected drive is '$DRIVE'.\n
 Press enter to continue...\n'
 dd if=/dev/zero of=/dev/$DRIVE  bs=512  count=1
@@ -37,28 +39,28 @@ parted -a opt /dev/$DRIVE mkpart primary 512 100%
 mkfs.fat -F32 /dev/${DRIVE}1
 mkfs.ext4 /dev/${DRIVE}2
 # Updating and syncing mirrorlist
-echo -e "---- Updating and syncing mirrorlist ---- \n"
+echo -e "\n---- Updating and syncing mirrorlist ----"
 pacman --noconfirm -Syy reflector
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 reflector -c "Australia" -f 5 -l 5 -n 5 --save /etc/pacman.d/mirrorlist
 # Installing Arch
-echo -e "---- Installing Arch ---- \n"
+echo -e "\n---- Installing Arch ----"
 mount /dev/sda2 /mnt
 pacstrap /mnt base linux linux-firmware vim nano
 genfstab -U /mnt >> /mnt/etc/fstab
-echo -e "---- Chroot into Arch ---- \n"
+echo -e "\n---- Chroot into Arch ----"
 arch-chroot /mnt
-echo -e "---- Setting up Timezones ---- \n"
+echo -e "\n---- Setting up Timezones ----"
 echo $TIMEZONE
 timedatectl set-timezone $TIMEZONE
-echo -e "---- Configuring locale ---- \n"
+echo -e "\n---- Configuring locale ----"
 locale-gen
 echo LANG=en_AU.UTF-8 > /etc/locale.conf
 export LANG=en_AU.UTF-8
-echo -e "---- Configuring Hostname and Networking ---- \n"
+echo -e "\n---- Configuring Hostname and Networking ----"
 echo $DEVICE > /etc/hostname
 touch /etc/hosts
 echo -e '127.0.0.1  localhost\n::1  localhost\n127.0.1.1    '$DEVICE'' > /etc/hosts
-echo -e "---- Enter in root password ---- \n"
+echo -e "\n---- Enter in root password ----"
 passwd
-echo -e "temp end"
+echo -e "\ntemp end"
